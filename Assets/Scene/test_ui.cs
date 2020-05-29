020815d826a907eef46b2e0274baeb410cf96812
zhangqi <txqc4@163.com>
2020-05-29 11:55:33 +0800

    添加sdk面板

---
+++ Assets/Scene/test_ui.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zby;
+
+public class test_ui : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        CheckSdkPanel();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void CheckSdkPanel()
+    {
+        Transform trans = this.transform.Find("SdkPanel");
+        TestSdk_component tc = new TestSdk_component();
+        bool  ret = tc.InitUiComponent(trans);
+        ZLog.I(this, "check SdkPanel result {0}", ret);
+    }
+}
