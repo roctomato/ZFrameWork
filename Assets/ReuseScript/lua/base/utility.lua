e4f38aca1e152a9583ad1ac74e4eaff51239b969
roc <roctomato@gmail.com>
2021-02-04 17:57:59 +0800

    添加split函数

--- Assets/ReuseScript/lua/base/utility.lua
+++ Assets/ReuseScript/lua/base/utility.lua
@@ -15,6 +15,23 @@ local _utility ={
         return nil        
     end,
 
+    Split = function (szFullString, szSeparator)  
+        local nFindStartIndex = 1  
+        local nSplitIndex = 1  
+        local nSplitArray = {}  
+        while true do  
+           local nFindLastIndex = string.find(szFullString, szSeparator, nFindStartIndex)  
+           if not nFindLastIndex then  
+            nSplitArray[nSplitIndex] = string.sub(szFullString, nFindStartIndex, string.len(szFullString))  
+            break  
+           end  
+           nSplitArray[nSplitIndex] = string.sub(szFullString, nFindStartIndex, nFindLastIndex - 1)  
+           nFindStartIndex = nFindLastIndex + string.len(szSeparator)  
+           nSplitIndex = nSplitIndex + 1  
+        end  
+        return nSplitArray  
+    end,  
+    
 }
 
 return _utility
